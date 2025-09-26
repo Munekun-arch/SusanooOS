c7c7f20a6c72592213f55f9b75b0632d0069bc66
Munekun-arch <view_root_sea@yahoo.co.jp>
2025-09-26 21:54:32 +0900

    Add key wait in HelloLoader (display Hello until key press)

--- SusanooLoader/HelloLoader.c
+++ SusanooLoader/HelloLoader.c
@@ -9,7 +9,16 @@ UefiMain (
   IN EFI_SYSTEM_TABLE *SystemTable
   )
 {
+  EFI_INPUT_KEY Key;
+
   Print(L"Hello SusanooOS Loader!\n");
+  Print(L"Press any key to continue...\n");
+
+  // キー入力があるまで待機
+  while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) == EFI_NOT_READY) {
+    gBS->Stall(100000); // 100ms 待機
+  }
+
   return EFI_SUCCESS;
 }
 
