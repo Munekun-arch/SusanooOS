b0d5c0b2e6af17f1adf3c29d41e6788882beec65
Munekun-arch <view_root_sea@yahoo.co.jp>
2025-09-27 03:42:40 +0900

    Add GOP red background and white square with key wait in HelloLoader

--- SusanooLoader/HelloLoader.c
+++ SusanooLoader/HelloLoader.c
@@ -1,7 +1,7 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
-#include <Protocol/GraphicsOutput.h>   // ← これが必要！
+#include <Protocol/GraphicsOutput.h>
 
 EFI_STATUS
 EFIAPI
@@ -10,38 +10,50 @@ UefiMain (
   IN EFI_SYSTEM_TABLE *SystemTable
   )
 {
-  EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
   EFI_STATUS Status;
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
 
-  // GOP を探す
+  // GOP プロトコルを取得
   Status = gBS->LocateProtocol(&gEfiGraphicsOutputProtocolGuid, NULL, (VOID**)&Gop);
   if (EFI_ERROR(Status)) {
-    Print(L"GOP not found!\n");
+    Print(L"Unable to locate GOP\n");
     return Status;
   }
 
-  // 真っ赤に塗りつぶす
-  UINT32 Red = 0x00FF0000;
   UINTN Width  = Gop->Mode->Info->HorizontalResolution;
   UINTN Height = Gop->Mode->Info->VerticalResolution;
+  UINTN PixelsPerScanLine = Gop->Mode->Info->PixelsPerScanLine;
+
   UINT32 *FrameBuffer = (UINT32*)Gop->Mode->FrameBufferBase;
 
+  // 背景を真っ赤に塗る
+  UINT32 Red = 0x00FF0000;  // BGRA (赤)
   for (UINTN y = 0; y < Height; y++) {
     for (UINTN x = 0; x < Width; x++) {
-      FrameBuffer[y * Gop->Mode->Info->PixelsPerScanLine + x] = Red;
+      FrameBuffer[y * PixelsPerScanLine + x] = Red;
     }
   }
 
+  // 中央に白い四角を描画
+  UINTN BoxSize = 200;
+  UINTN BoxX0 = (Width - BoxSize) / 2;
+  UINTN BoxY0 = (Height - BoxSize) / 2;
+  UINT32 White = 0x00FFFFFF; // 白
+  for (UINTN y = BoxY0; y < BoxY0 + BoxSize; y++) {
+    for (UINTN x = BoxX0; x < BoxX0 + BoxSize; x++) {
+      FrameBuffer[y * PixelsPerScanLine + x] = White;
+    }
+  }
+
+  // 文字を表示
   Print(L"Hello SusanooOS Loader!\n");
   Print(L"Press any key to exit...\n");
 
   // キー入力待ち
   EFI_INPUT_KEY Key;
-  while (1) {
-    Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
-    if (!EFI_ERROR(Status)) {
-      break;
-    }
+  SystemTable->ConIn->Reset(SystemTable->ConIn, FALSE);
+  while ((Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY) {
+    gBS->Stall(100000); // 0.1秒待機
   }
 
   return EFI_SUCCESS;
